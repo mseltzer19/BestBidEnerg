1c61a9682abb070b6e526859e6094ccf9126d211
mseltzer19 <seltzer.19@buckeyemail.osu.edu>
2017-04-09 15:20:51 -0500

    Add project files.

---
+++ src/ConsoleApp1/Entities/BestBidUserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1.Entities
+{
+    public class BestBidUserDbContext : DbContext
+    {
+        public DbSet<BestBidUser> BestBidUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(
+                @"Server = (localdb)\MSSQLLocalDB;Database=BestBidDb;"
+                + "Trusted_Connection=True;");
+        }
+    }
+}
---
+++ src/ConsoleApp1/Migrations/BestBidUserDbContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ConsoleApp1.Entities;
+
+namespace ConsoleApp1.Migrations
+{
+    [DbContext(typeof(BestBidUserDbContext))]
+    partial class BestBidUserDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ConsoleApp1.Entities.BestBidUser", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("UserRole");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("BestBidUsers");
+                });
+        }
+    }
+}
